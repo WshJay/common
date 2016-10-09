4186ea267b0b509634d9ef1379f2e45239c92abd
wsh <hzwsh@tairanchina.com>
2016-10-09 15:53:19 +0800

    整合Dubbo服务

--- common-dubbo-provider/src/main/java/org/wsh/common/provider/test/Provider.java
+++ common-dubbo-provider/src/main/java/org/wsh/common/provider/test/Provider.java
@@ -1,13 +1,11 @@
 package org.wsh.common.provider.test;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class Provider {
 
 	public static void main(String[] args) throws Exception {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/bean.xml","classpath:/application-provider.xml");
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:/provider-bean.xml");
 		context.start();
 
 		System.in.read(); // 按任意键退出
--- common-test/src/test/java/org/wsh/common/test/dubbo/ConsumerTest.java
+++ common-test/src/test/java/org/wsh/common/test/dubbo/ConsumerTest.java
@@ -1,5 +1,8 @@
 package org.wsh.common.test.dubbo;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.wsh.common.consumer.service.DemoService;
 import org.junit.Test;
 
@@ -11,6 +14,8 @@ import javax.annotation.Resource;
  * @comments: 对此类的描述，可以引用系统设计中的描述
  * @since Date： 2016-09-10 15:30
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/application-consumer.xml"})
 public class ConsumerTest {
 
     @Resource
