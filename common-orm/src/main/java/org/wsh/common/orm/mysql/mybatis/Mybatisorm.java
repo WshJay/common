917ff2c41bc3251f7c9f3ba5315ab75317e7e249
wsh <hzwsh@tairanchina.com>
2017-05-19 15:05:20 +0800

    ORM更新

--- common-orm/src/main/java/org/wsh/common/orm/mysql/mybatis/Mybatisorm.java
+++ common-orm/src/main/java/org/wsh/common/orm/mysql/mybatis/Mybatisorm.java
@@ -40,11 +40,11 @@ public class Mybatisorm {
 		builder.setUrl("jdbc:mysql://47.88.25.246:3306/common");//数据库链接
 		builder.setName("root");//数据库用户名
 		builder.setPwd("wsh0704");//数据库密码
-		builder.setPojoPackage("org.wsh.common.model.follow");//pojo包地址
-		builder.setDaoPackage("org.wsh.common.dao.follow");//dao包地址
-		builder.setServicePackage("org.wsh.common.service.api.follow");
-		builder.setServiceImplPackage("org.wsh.common.service.impl.follow");
-		builder.setServiceTestPackage("org.wsh.common.test.service.follow");
+		builder.setPojoPackage("org.wsh.common.model.redpacket");//pojo包地址
+		builder.setDaoPackage("org.wsh.common.dao.redpacket");//dao包地址
+		builder.setServicePackage("org.wsh.common.service.api.redpacket");
+		builder.setServiceImplPackage("org.wsh.common.service.impl.redpacket");
+		builder.setServiceTestPackage("org.wsh.common.test.service.redpacket");
 		outPathMap.put(OutPathKey.DEFULT,"F:/TableModel/");
 		outPathMap.put(OutPathKey.DO,PROJECT_PATH + "\\model\\");
 		outPathMap.put(OutPathKey.DAO,PROJECT_PATH + "\\mapper\\");
@@ -53,7 +53,7 @@ public class Mybatisorm {
 		outPathMap.put(OutPathKey.SERVICE_IMPL,PROJECT_PATH + "\\service\\impl\\");
 		outPathMap.put(OutPathKey.SERVICE_TEST,PROJECT_PATH + "\\service\\test\\");
 		builder.setOutPathMap(outPathMap);//生成文件地址
-		builder.setTableName("file");//数据库表名 %全部
+		builder.setTableName("red_packet_%");//数据库表名 %全部
 		return builder.build();
 	}
 
--- common-orm/src/main/java/org/wsh/common/orm/mysql/mybatis/build/BuildServiceTest.java
+++ common-orm/src/main/java/org/wsh/common/orm/mysql/mybatis/build/BuildServiceTest.java
@@ -1,17 +1,16 @@
 package org.wsh.common.orm.mysql.mybatis.build;
 
 import org.wsh.common.orm.mysql.mybatis.base.AbstractBuildFactory;
+import org.wsh.common.orm.mysql.mybatis.bean.BigAndSmall;
 import org.wsh.common.orm.mysql.mybatis.bean.Column;
 import org.wsh.common.orm.mysql.mybatis.bean.Table;
 import org.wsh.common.orm.mysql.mybatis.bean.TableWapper;
 import org.wsh.common.orm.mysql.mybatis.enums.OutPathKey;
 import org.wsh.common.orm.mysql.mybatis.util.Util;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
 import static org.wsh.common.orm.mysql.mybatis.util.Util.parseDate;
 
 /**
@@ -34,18 +33,56 @@ public class BuildServiceTest extends AbstractBuildFactory {
         String servicePackage = tableWapper.getServicePackage();
         String serviceImplPackage = tableWapper.getServiceImplPackage();
         String serviceTestPackage = tableWapper.getServiceTestPackage();
-
+        Table table = tableWapper.getTable();
+        List<BigAndSmall> column = new ArrayList<BigAndSmall>();
+        List<Column> columnList = table.getColumns();
         String outPath = tableWapper.getOutPathMap().get(OutPathKey.DEFULT);
         if(tableWapper.getOutPathMap().get(getOutPath()) != null){
             outPath = tableWapper.getOutPathMap().get(getOutPath());
         }
-        Table table = tableWapper.getTable();
         String tableName = table.getName();
         String bigDoName = Util.getUpperHumpName(tableName)+"DO";
         String minDoName = Util.getHumpName(tableName)+"DO";
         String bigServiceName = Util.getUpperHumpName(tableName)+"Service";
         String minServiceName = Util.getHumpName(tableName)+"Service";
         String bigServiceTestName = bigServiceName+"Test";
+        for (Column co : columnList) {
+            Column colu = new Column();
+            BigAndSmall bigSmall = new BigAndSmall();
+            String type = co.getType();
+            if (type.equalsIgnoreCase("DATETIME") || type.equalsIgnoreCase("DATE") || type.equalsIgnoreCase("TIME")
+                    || type.equalsIgnoreCase("TIMESTAMP")) {
+                type = "Date";
+            } else if (type.equalsIgnoreCase("VARCHAR") || type.equalsIgnoreCase("CHAR")
+                    || type.equalsIgnoreCase("BLOB") || type.equalsIgnoreCase("TEXT")
+                    || type.equalsIgnoreCase("LONGBLOB") || type.equalsIgnoreCase("LONGTEXT")) {
+                type = "String";
+            } else if (type.equalsIgnoreCase("ENUM")) {
+                type = Util.getUpperHumpName(co.getName());
+            } else if (type.equalsIgnoreCase("INT") || type.equalsIgnoreCase("TINYINT")) {
+                type = "int";
+            } else if (type.equalsIgnoreCase("BIGINT")) {
+                type = "Long";
+            } else if (type.equalsIgnoreCase("DOUBLE")) {
+                type = "Double";
+            } else if (type.equalsIgnoreCase("FLOAT")) {
+                type = "Float";
+            } else if (type.equalsIgnoreCase("INTEGER")) {
+                type = "Integer";
+            }else if (type.equalsIgnoreCase("DECIMAL")) {
+                type = "BigDecimal";
+            }
+            if(Util.getHumpName(co.getName()).equalsIgnoreCase("id")||
+                    Util.getHumpName(co.getName()).equalsIgnoreCase("gmtCreated")||
+                    Util.getHumpName(co.getName()).equalsIgnoreCase("gmtModified")) continue;
+            colu.setType(type);
+            colu.setRemark(co.getRemark());
+            colu.setName(Util.getHumpName(co.getName()));
+            bigSmall.setBigName(Util.getUpperHumpName(co.getName()));
+            bigSmall.setSmallName(Util.getHumpName(co.getName()));
+            bigSmall.setType(type);
+            column.add(bigSmall);
+        }
         Map<String,Object> map = new HashMap<String,Object>();
         map.put("servicePackage", servicePackage);
         map.put("serviceTestPackage", serviceTestPackage);
@@ -53,6 +90,7 @@ public class BuildServiceTest extends AbstractBuildFactory {
         map.put("minDoName", minDoName);
         map.put("bigServiceName", bigServiceName);
         map.put("minServiceName", minServiceName);
+        map.put("columnList", column);
         map.put("date", parseDate(new Date(),"yyyy-MM-dd HH:mm:ss"));
         Util.writeCode("serviceTest", map,outPath+bigServiceTestName+".java/");
     }
