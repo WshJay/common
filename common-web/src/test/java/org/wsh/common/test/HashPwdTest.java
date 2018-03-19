4e7894275d57af0883391cf6b53e1237d9829c7b
wangshuhui <675783578@qq.com>
2018-03-19 23:57:48 +0800

    补充遗漏代码

--- common-web/src/test/java/org/wsh/common/test/HashPwdTest.java
+++ common-web/src/test/java/org/wsh/common/test/HashPwdTest.java
@@ -8,7 +8,7 @@ public class HashPwdTest {
 
 	@Test
 	public void test(){
-		HashPwd.HashPassword hashPassword = HashPwd.encrypt("123456");
+		HashPwd.HashPassword hashPassword = HashPwd.encrypt("xiaohui0704");
 		System.out.println("密码:" + hashPassword.password + "密钥:" + hashPassword.salt);
 	}
 }
